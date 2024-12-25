b2e11d9b154833db4a45448f15989254a92f7212
AnastasiiaBochkareva <120329458+AnastasiiaBochkareva@users.noreply.github.com>
2024-12-25 15:47:15 +0400

    change header

--- script.js
+++ script.js
@@ -1,20 +1,60 @@
-const openMenu = document.querySelector(".open-menu");
-const closeMenu = document.querySelector(".close-menu");
+// const openMenu = document.querySelector(".open-menu");
+// const closeMenu = document.querySelector(".close-menu");
+// const menu = document.querySelector(".menu");
+
+// document.addEventListener("click", (event) => {
+//     const element = event.target;
+//     if (element.closest(".open-menu")) {
+//         openMenu.classList.add("hidden");
+//         openMenu.classList.remove("visible");
+//         closeMenu.classList.add("visible");
+//         closeMenu.classList.remove("hidden");
+//         if (menu.classList.contains("opened")) {
+//             menu.classList.remove("opened");
+//         } else {
+//             menu.classList.add("opened");
+//         }
+//     }
+//     if (element.closest(".close-menu")) {
+//         openMenu.classList.add("visible");
+//         closeMenu.classList.add("hidden");
+//         openMenu.classList.remove("hidden");
+//         closeMenu.classList.remove("visible");
+//         if (menu.classList.contains("opened")) {
+//             menu.classList.remove("opened");
+//         }
+//     }
+// });
+
+// Кэшируем элементы
+const openMenuButton = document.querySelector(".open-menu");
+const closeMenuButton = document.querySelector(".close-menu");
 const menu = document.querySelector(".menu");
 
-function toggleMenu() {
-    if (menu.classList.contains("opened")) {
-        menu.classList.remove("opened");
-        openMenu.classList.remove("hidden");
-        closeMenu.classList.add("visible");
-    } else {
-        menu.classList.add("opened");
-        openMenu.classList.add("hidden");
-        closeMenu.classList.remove("hidden");
-    }
-}
+// Функции для управления состоянием меню
+const openMenu = () => {
+    toggleVisibility(openMenuButton, false);
+    toggleVisibility(closeMenuButton, true);
+    menu.classList.add("opened");
+};
 
-if (openMenu && closeMenu && menu) {
-    openMenu.addEventListener("click", toggleMenu);
-    closeMenu.addEventListener("click", toggleMenu);
-}
+const closeMenu = () => {
+    toggleVisibility(openMenuButton, true);
+    toggleVisibility(closeMenuButton, false);
+    menu.classList.remove("opened");
+};
+
+// Утилита для управления видимостью
+const toggleVisibility = (element, isVisible) => {
+    element.classList.toggle("visible", isVisible);
+    element.classList.toggle("hidden", !isVisible);
+};
+
+// Основной обработчик событий
+document.addEventListener("click", (event) => {
+    if (event.target.closest(".open-menu")) {
+        openMenu();
+    } else if (event.target.closest(".close-menu")) {
+        closeMenu();
+    }
+});
